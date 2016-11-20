47f4856f780000779103dfa8997f24a6d41ddcdc
Brady Trainor <mail@bradyt.com>
2016-11-20 14:56:39 -0800

    Saves 2014 csv

--- make-csvs.py
+++ make-csvs.py
@@ -1,16 +1,24 @@
 
 import re
 import glob, os
-os.chdir(os.path.expanduser("~/my-tax-tools"))
 
 import get_tables
 
-for infile in glob.glob("xmls/*.xml"):
-    print(infile)
-    # base = re.sub(".*--([0-9]{4}).xml", "\\1", infile)
-    # outfile = 'csvs/tax-table-' + base + '.csv'
-    # print('Reading $infile...')
-    # get_tables.xml_to_csv(infile, outfile)
-    # print('Wrote to $outfile')
+def get_csv_filename(infile):
+    base = re.sub(".*--([0-9]{4}).xml", "\\1", infile)
+    return 'csvs/tax-table-' + base + '.csv'
+
+xml_files = [
+    # 'xmls/i1040gi--2013.xml',
+    # 'xmls/i1040gi--2014.xml',
+    'xmls/i1040gi--2015.xml'
+]
+
+for infile in xml_files:
+    outfile = get_csv_filename(infile)
+    print('Reading ' + infile + '...')
+    get_tables.xml_to_csv(infile, outfile)
+    print('Wrote to ' + outfile)
 
 print('Done!')
+    
